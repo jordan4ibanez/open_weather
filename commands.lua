4e933f52c6a487a2b778b5bd4e0ad4ea0d42965d
jordan4ibanez <jordan4ibanez@gmail.com>
2016-12-30 23:08:00 -0500

    add snow

--- commands.lua
+++ commands.lua
@@ -10,7 +10,7 @@ can either type the word or number to get the state if you have server privelage
 
 ]]--
 minetest.register_chatcommand("weather", {
-	params = "<weather type>",
+	params = "<precipitation force>",
 	description = "Set the weather",
 	privs = {server = true},
 	func = function( name, weather)
@@ -30,5 +30,29 @@ minetest.register_chatcommand("weather", {
 			minetest.chat_send_player(name, weather.." is not a state!")
 		end
 		
+	end,
+})
+
+--[[
+sets rain or snow
+
+0 = rain
+1 = snow
+
+]]--
+minetest.register_chatcommand("cold", {
+	params = "<weather type>",
+	description = "Set the weather",
+	privs = {server = true},
+	func = function( name, weather)
+		if weather == "rain" or weather == "0" then
+			open_weather.type = 0
+			minetest.chat_send_player(name, "Setting weather to rain!")
+		elseif weather == "snow" or weather == "1" then
+			open_weather.type = 1
+			minetest.chat_send_player(name, "Setting weather to snow!")
+		else
+			minetest.chat_send_player(name, weather.." is not a weather type!")
+		end
 	end,
 })
--- init.lua
+++ init.lua
@@ -34,6 +34,13 @@ open_weather.spawner_timer = 0
 open_weather.sheltered = {}
 --the players skybox
 open_weather.skybox = {}
+--the weather type (for now, in the future will use humidity of the biome)
+--0 = rain
+--1 = snow
+open_weather.type = 0
+--the old type
+open_weather.old_type = 0
+
 --this sets the local particle spawner to the player
 open_weather.set_spawner = function(player)
 	
@@ -48,7 +55,8 @@ open_weather.set_spawner = function(player)
 	
 	
 	--stop sounds
-	if open_weather.state == 0 and open_weather.sounds[name] ~= nil then
+	print(open_weather.type, open_weather.old_type)
+	if (open_weather.state == 0 or open_weather.type ~= open_weather.old_type) and open_weather.sounds[name] ~= nil then
 		--stop sound
 		print("stopping sound")
 		minetest.sound_stop(open_weather.sounds[name])
@@ -59,7 +67,8 @@ open_weather.set_spawner = function(player)
 		print("removing skybox")
 		player:set_sky({r=0, g=0, b=0},"regular",{})
 	end
-		
+	
+	
 	--return if clear to not waste resources
 	if open_weather.state == 0 then
 		return
@@ -68,8 +77,10 @@ open_weather.set_spawner = function(player)
 	--variables for particle spawners
 	local pos = player:getpos()
 	local id = nil
-	local is_sheltered = not minetest.line_of_sight(pos, {x=pos.x,y=pos.y+40,z=pos.z}, 1) --will still rain if in huge house
-		
+	local is_sheltered = not (minetest.get_node_light(pos, 0.5) == 15)
+	
+	
+	
 	--update sounds
 	if open_weather.sounds[name] ~= nil and is_sheltered ~= open_weather.sheltered[name] then
 		--stop sound
@@ -79,24 +90,49 @@ open_weather.set_spawner = function(player)
 	end
 	
 	--play weather sounds
-	if open_weather.state ~= 0 and open_weather.sounds[name] == nil then
-		local id 
-		if is_sheltered == false then
-			print("playing outside sound")
-			id = minetest.sound_play("open_weather_rain_outside", {
-				to_player = name,
-				gain = 1.0,
-				loop = true,
-			})
-		elseif is_sheltered == true then
-			id = minetest.sound_play("open_weather_rain_inside", {
-				to_player = name,
-				gain = 1.0,
-				loop = true,
-			})
+	--rain
+	if open_weather.type == 0 then
+		if open_weather.state ~= 0 and open_weather.sounds[name] == nil then
+			local id 
+			if is_sheltered == false then
+				print("playing outside sound")
+				id = minetest.sound_play("open_weather_rain_outside", {
+					to_player = name,
+					gain = 1.0,
+					loop = true,
+				})
+			elseif is_sheltered == true then
+				id = minetest.sound_play("open_weather_rain_inside", {
+					to_player = name,
+					gain = 1.0,
+					loop = true,
+				})
+			end
+			
+			open_weather.sounds[name] = id
 		end
-		
-		open_weather.sounds[name] = id
+	--snow
+	elseif open_weather.type == 1 then
+		if open_weather.state ~= 0 and open_weather.sounds[name] == nil then
+			local id 
+			if is_sheltered == false then
+				print("playing outside sound")
+				id = minetest.sound_play("open_weather_snow_outside", {
+					to_player = name,
+					gain = 1.0,
+					loop = true,
+				})
+			elseif is_sheltered == true then
+				id = minetest.sound_play("open_weather_snow_inside", {
+					to_player = name,
+					gain = 1.0,
+					loop = true,
+				})
+			end
+			
+			open_weather.sounds[name] = id
+		end
+	
 	end
 	
 	--set particle spawner
@@ -109,32 +145,68 @@ open_weather.set_spawner = function(player)
 		elseif open_weather.state == 2 then
 			particle_amount = 700
 		elseif open_weather.state == 3 then
-			particle_amount = 1200
+			particle_amount = 1400
 		end
+		
+		--set the y velocity
+		local y_velocity
+		local particle
+		
+		if open_weather.type == 0 then
+			particle = "open_weather_rain_drop.png"
+			if open_weather.state == 1 then
+				y_velocity = -20
+			elseif open_weather.state == 2 then
+				y_velocity = -30
+			elseif open_weather.state == 3 then
+				y_velocity = -40
+			end	
+		elseif open_weather.type == 1 then
+			particle = "open_weather_snow_flake.png"
+			if open_weather.state == 1 then
+				y_velocity = -3
+			elseif open_weather.state == 2 then
+				y_velocity = -5
+			elseif open_weather.state == 3 then
+				y_velocity = -7
+			end
+		end
+		
+		local h_acceleration = 0
+		--set the horizontal acceleration
+		if open_weather.type == 1 then
+			if open_weather.state == 1 then
+				h_acceleration = 3
+			elseif open_weather.state == 2 then
+				h_acceleration = 6
+			elseif open_weather.state == 3 then
+				h_acceleration = 9
+			end
+		end
+			
 		
 		id = minetest.add_particlespawner({
 			amount = particle_amount,
 			time = 0,
 			minpos = {x=pos.x-20, y=pos.y-20, z=pos.z-20},
 			maxpos = {x=pos.x+20, y=pos.y+20, z=pos.z+20},
-			minvel = {x=0, y=-20, z=0},
-			maxvel = {x=0, y=-20, z=0},
-			minacc = {x=0, y=0, z=0},
-			maxacc = {x=0, y=0, z=0},
+			minvel = {x=0, y=y_velocity, z=0},
+			maxvel = {x=0, y=y_velocity, z=0},
+			minacc = {x=-h_acceleration, y=0, z=-h_acceleration},
+			maxacc = {x=h_acceleration, y=0, z=h_acceleration},
 			minexptime = 1,
 			maxexptime = 1,
 			minsize = 1,
 			maxsize = 1,
 			collisiondetection = true,
 			vertical = true,
-			texture = "open_weather_rain_drop.png",
+			texture = particle,
 			playername = name,
 		})
 	end
 	
 	--set skybox to state
-	if open_weather.skybox[name] ~= nil or open_weather.state ~= open_weather.skybox[name] then
-		print("setting player's skybox")
+	if open_weather.skybox[name] == nil or open_weather.state ~= open_weather.skybox[name] then
 		open_weather.skybox[name] = open_weather.state
 		--darkness depends on weather
 		local rgb
@@ -163,13 +235,17 @@ minetest.register_globalstep(function(dtime)
 	open_weather.spawner_timer = open_weather.spawner_timer + dtime
 	
 	--update the state
-	if open_weather.spawner_timer > 1 then
+	if open_weather.spawner_timer > 0.2 then
 		--print("update spawners")
 		for _,player in ipairs(minetest.get_connected_players()) do
 			open_weather.set_spawner(player)
 		end
 		open_weather.spawner_timer = 0
+		--remember the old type
+		open_weather.old_type = open_weather.type
 	end
+	
+	
 
 end)
 
